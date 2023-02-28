597b7355608b9c8b08570b1b79c32894e8874f42
NK7 <frostbreker@gmail.com>
2023-02-28 14:16:09 +0100

    Init 1.18.2

---
+++ src/main/java/fr/frostbreker/onetwenty/OneTwentyMod.java
@@ -0,0 +1,24 @@
+package fr.frostbreker.onetwenty;
+
+import fr.frostbreker.onetwenty.utils.Reference;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(Reference.MOD_ID)
+public class OneTwentyMod {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public OneTwentyMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+}
---
+++ src/main/java/fr/frostbreker/onetwenty/utils/Reference.java
@@ -0,0 +1,5 @@
+package fr.frostbreker.onetwenty.utils;
+
+public class Reference {
+    public static final String MOD_ID = "onetwenty";
+}
